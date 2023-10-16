385c6cdb0b80527fd6bc96ef0cf5d7ef3e330eb8
ZhytnkovskaIryna <iryna.zhytnikovska@gmail.com>
2023-10-16 19:11:45 +0300

    Added await before each expect action

--- test/specs/HomeBasic.spec.js
+++ test/specs/HomeBasic.spec.js
@@ -5,18 +5,18 @@ describe("Basic tests for Expense Tracker home page",()=> {
     it("Verify title of the page",async()=>{
         await pages('home').open();
         const pageTitle = await browser.getTitle();
-        expect(pageTitle).toEqual("Essential JS 2 for JavaScript (ES5) - Expense Tracker");
+        await expect(pageTitle).toEqual("Essential JS 2 for JavaScript (ES5) - Expense Tracker");
     })
 
     it("Verify that balance is displayed in side menu",async()=>{
-        expect(await pages('home').sideMenu.balance.getText()).toEqual("$5,240");
+        await expect(await pages('home').sideMenu.balance.getText()).toEqual("$5,240");
     })
 
     it("Verify that transaction dashboard is opened when this menu option is selected",async()=>{
         await pages('home').sideMenu.item('transactions').click();
         const elem = await pages('transactions').transaction.item('addTransaction');
         await elem.waitForDisplayed();
-        expect(await elem.isDisplayed()).toEqual(true);
+        await expect(await elem.isDisplayed()).toEqual(true);
 
     })
 
@@ -24,7 +24,7 @@ describe("Basic tests for Expense Tracker home page",()=> {
         await pages('home').sideMenu.item('about').click();
         const elem = await $('.about-heading');
         await elem.waitForDisplayed();
-        expect(await elem.isDisplayed()).toEqual(true);
+        await expect(await elem.isDisplayed()).toEqual(true);
 
     })
 
@@ -32,6 +32,6 @@ describe("Basic tests for Expense Tracker home page",()=> {
         await pages('transactions').open();
         await pages('transactions').transaction.item('addTransaction').click();
         await pages('transactions').transaction.item('modal').waitForDisplayed();
-        expect( await pages('transactions').transaction.item('modal').isDisplayed()).toEqual(true);
+        await expect( await pages('transactions').transaction.item('modal').isDisplayed()).toEqual(true);
     })
 })
