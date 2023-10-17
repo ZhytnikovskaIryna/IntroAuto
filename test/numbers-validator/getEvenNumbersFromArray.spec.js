50a9e50d07bb87779499d085ef2f475a2ebb33c2
ZhytnkovskaIryna <iryna.zhytnikovska@gmail.com>
2023-10-17 23:37:07 +0300

    corrected eslint config file

--- test/numbers-validator/getEvenNumbersFromArray.spec.js
+++ test/numbers-validator/getEvenNumbersFromArray.spec.js
@@ -1,41 +1,43 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-undef */
 import { afterEach } from 'mocha';
-import NumbersValidator from '../../app/Numbers_Validator.js';
 import { expect } from 'chai';
+import NumbersValidator from '../../app/Numbers_Validator.js';
 
-describe('getEvenNumbersFromArray testing positive and negative scenarios', ()=> {
+describe('getEvenNumbersFromArray testing positive and negative scenarios', () => {
   let validator;
-  beforeEach(()=>{
+  beforeEach(() => {
     validator = new NumbersValidator();
   });
 
-  afterEach(()=>{
-    validator=null;
+  afterEach(() => {
+    validator = null;
   });
 
-  it('should return array of even numbers when array of numbers is provided', ()=>{
-    const validationResults = validator.getEvenNumbersFromArray([5,7,6,7,8]);
-    expect(validationResults).to.be.eql([6,8]);
+  it('should return array of even numbers when array of numbers is provided', () => {
+    const validationResults = validator.getEvenNumbersFromArray([5, 7, 6, 7, 8]);
+    expect(validationResults).to.be.eql([6, 8]);
   });
 
-  it('should return empty array if provided array is empty', ()=>{
+  it('should return empty array if provided array is empty', () => {
     const validationResults = validator.getEvenNumbersFromArray([]);
     expect(validationResults).to.be.eql([]);
   });
 
-  it('should return an empty array if in provided array has all numbers but there are no even', ()=>{
-    const validationResults = validator.getEvenNumbersFromArray([5,7,9,13,57]);
+  it('should return an empty array if in provided array has all numbers but there are no even', () => {
+    const validationResults = validator.getEvenNumbersFromArray([5, 7, 9, 13, 57]);
     expect(validationResults).to.be.eql([]);
   });
 
-  it('should throw an error when provided value is not an array', ()=>{
-    expect(()=>{
+  it('should throw an error when provided value is not an array', () => {
+    expect(() => {
       validator.getEvenNumbersFromArray('4');
     }).to.throw('[4] is not an array of "Numbers"');
   });
 
-  it('should throw an error when provided value is array with not only numbers', ()=>{
-    expect(()=>{
-      validator.getEvenNumbersFromArray([6,7,"j"]);
+  it('should throw an error when provided value is array with not only numbers', () => {
+    expect(() => {
+      validator.getEvenNumbersFromArray([6, 7, 'j']);
     }).to.throw('[6,7,j] is not an array of "Numbers"');
   });
 });
--- test/numbers-validator/isAllNumbers.spec.js
+++ test/numbers-validator/isAllNumbers.spec.js
@@ -1,34 +1,36 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-undef */
 import { afterEach } from 'mocha';
-import NumbersValidator from '../../app/Numbers_Validator.js';
 import { expect } from 'chai';
+import NumbersValidator from '../../app/Numbers_Validator.js';
 
-describe('isAllNumbers  testing positive and negative scenarios', ()=> {
+describe('isAllNumbers  testing positive and negative scenarios', () => {
   let validator;
-  beforeEach(()=>{
+  beforeEach(() => {
     validator = new NumbersValidator();
   });
 
-  afterEach(()=>{
-    validator=null;
+  afterEach(() => {
+    validator = null;
   });
 
-  it('should return true when provided with array where all elements are numbers', ()=>{
-    const validationResults = validator.isAllNumbers([5,2,1]);
+  it('should return true when provided with array where all elements are numbers', () => {
+    const validationResults = validator.isAllNumbers([5, 2, 1]);
     expect(validationResults).to.be.equal(true);
   });
 
-  it('should return false when provided with array that has array inside', ()=>{
-    const validationResults = validator.isAllNumbers([5,2,1,[6,9]]);
+  it('should return false when provided with array that has array inside', () => {
+    const validationResults = validator.isAllNumbers([5, 2, 1, [6, 9]]);
     expect(validationResults).to.be.equal(false);
   });
 
-  it('should return false when provided with not all numbers', ()=>{
-    const validationResults = validator.isAllNumbers(['g',4,6]);
+  it('should return false when provided with not all numbers', () => {
+    const validationResults = validator.isAllNumbers(['g', 4, 6]);
     expect(validationResults).to.be.equal(false);
   });
 
-  it('should throw an error when provided value is not an array', ()=>{
-    expect(()=>{
+  it('should throw an error when provided value is not an array', () => {
+    expect(() => {
       validator.isAllNumbers('4');
     }).to.throw('[4] is not an array');
   });
