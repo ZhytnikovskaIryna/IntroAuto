82a4b8fcb90614576c49a53ae9454366db13dc07
ZhytnkovskaIryna <iryna.zhytnikovska@gmail.com>
2023-10-17 22:44:53 +0300

    fixed issue with failing tests

--- test/specs/Transactions.spec.js
+++ test/specs/Transactions.spec.js
@@ -4,14 +4,14 @@ const{pages}=require('../po');
 describe("Basic tests for Expense Tracker Transaction page",()=> {
 
     beforeEach(async function () {
-        transactionPage = await pages('transactions');
+        transactionPage = pages('transactions');
         await transactionPage.open();
       });
 
     it("Verify that modal window is opened when Add transaction is clicked on Transaction page",async()=>{
         await transactionPage.transaction.AddNewTransactionBtn.waitAndClick();
         await transactionPage.transaction.item('modal').waitForDisplayed();
-        await expect( await transactionPage.transaction.item('modal').isDisplayed()).toEqual(true);
+        await expect(await transactionPage.transaction.item('modal')).toBeDisplayed();
         await transactionPage.addTransaction.cancelButton.waitAndClick();
 
     })
@@ -21,7 +21,7 @@ describe("Basic tests for Expense Tracker Transaction page",()=> {
         await transactionPage.transaction.AddNewTransactionBtn.waitAndClick();
         await transactionPage.addTransaction.input('amount').setValue('100');
         await transactionPage.addTransaction.addButton.waitAndClick();
-        await expect(await transactionPage.transaction.item('itemsCount').getText()).toEqual("("+(parseInt(count.slice(1,-7))+1).toString()+" items)")
+        await expect(await transactionPage.transaction.item('itemsCount')).toHaveText("("+(parseInt(count.slice(1,-7))+1).toString()+" items)")
     })
 
     it("Verify that transaction can be deleted at Transaction page total count of transactions is decreased ",async()=>{
@@ -29,16 +29,15 @@ describe("Basic tests for Expense Tracker Transaction page",()=> {
         await transactionPage.transaction.item('checkbox').waitAndClick();
         await transactionPage.transaction.DeleteTransactionBtn.waitAndClick();
         await transactionPage.deleteTransactionDialog.DeleteDialogOkButton.waitAndClick();
-        await expect(await transactionPage.transaction.item('itemsCount').getText()).toEqual("("+(parseInt(count.slice(1,-7))-1).toString()+" items)")
+        await expect(await transactionPage.transaction.item('itemsCount')).toHaveText("("+(parseInt(count.slice(1,-7))-1).toString()+" items)");
     })
 
-    it("Verify that transaction is not deleted if Cancel button is presed in Delete Transaction dialog",async()=>{
+    it("Verify that transaction is not deleted if Cancel button is clicked in Delete Transaction dialog",async()=>{
         const count = await transactionPage.transaction.item('itemsCount').getText();    
-
         await transactionPage.transaction.item('checkbox').waitAndClick();
         await transactionPage.transaction.DeleteTransactionBtn.waitAndClick();
         await transactionPage.deleteTransactionDialog.DeleteDialogCancelButton.waitAndClick();
-        await expect(await transactionPage.transaction.item('itemsCount').getText()).toEqual(count);
+        await expect(await transactionPage.transaction.item('itemsCount')).toHaveText(count);
     })
 
     it("Verify that new transaction can be added with description ",async()=>{
@@ -47,7 +46,12 @@ describe("Basic tests for Expense Tracker Transaction page",()=> {
         await transactionPage.addTransaction.input('amount').setValue('100');
         await transactionPage.addTransaction.input('description').setValue('description random');
         await transactionPage.addTransaction.addButton.waitAndClick();
-        const descr = await transactionPage.transaction.item('description').getText();
-        await expect(descr).toEqual('description random');
+        await expect(await transactionPage.transaction.item('description')).toHaveText('description random');
+    })
+
+    it("Verify that checkbox is clickable",async()=>{
+        const elem = await transactionPage.transaction.item('checkbox');   
+        await expect(elem).toBeClickable();
+        
     })
 })
