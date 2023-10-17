04b15054ec0882e56d76a7a17373ea2a83ba1069
ZhytnkovskaIryna <iryna.zhytnikovska@gmail.com>
2023-10-18 01:16:29 +0300

    typo

--- test/specs/Transactions.spec.js
+++ test/specs/Transactions.spec.js
@@ -1,14 +1,10 @@
-
-const{pages}=require('../po');
+const{pages}=require('./../po');
 
 describe("Basic tests for Expense Tracker Transaction page",()=> {
 
-    beforeEach(async function () {
+    it("Verify that modal window is opened when Add transaction is clicked on Transaction page",async()=>{
         transactionPage = pages('transactions');
         await transactionPage.open();
-      });
-
-    it("Verify that modal window is opened when Add transaction is clicked on Transaction page",async()=>{
         await transactionPage.transaction.AddNewTransactionBtn.waitAndClick();
         await transactionPage.transaction.item('modal').waitForDisplayed();
         await expect(await transactionPage.transaction.item('modal')).toBeDisplayed();
@@ -16,7 +12,9 @@ describe("Basic tests for Expense Tracker Transaction page",()=> {
 
     })
 
-    it("Verify that new transaction can be added on Transaction page and +1 is add to the total count of transactions ",async()=>{
+    it("Verify that new transaction can be added at Transaction page and +1 is added to the total count of transactions ",async()=>{
+        transactionPage = pages('transactions');
+        await transactionPage.open();
         const count = await transactionPage.transaction.item('itemsCount').getText();    
         await transactionPage.transaction.AddNewTransactionBtn.waitAndClick();
         await transactionPage.addTransaction.input('amount').setValue('100');
@@ -25,6 +23,8 @@ describe("Basic tests for Expense Tracker Transaction page",()=> {
     })
 
     it("Verify that transaction can be deleted at Transaction page total count of transactions is decreased ",async()=>{
+        transactionPage = pages('transactions');
+        await transactionPage.open();
         const count = await transactionPage.transaction.item('itemsCount').getText();    
         await transactionPage.transaction.item('checkbox').waitAndClick();
         await transactionPage.transaction.DeleteTransactionBtn.waitAndClick();
@@ -33,6 +33,8 @@ describe("Basic tests for Expense Tracker Transaction page",()=> {
     })
 
     it("Verify that transaction is not deleted if Cancel button is clicked in Delete Transaction dialog",async()=>{
+        transactionPage = pages('transactions');
+        await transactionPage.open();
         const count = await transactionPage.transaction.item('itemsCount').getText();    
         await transactionPage.transaction.item('checkbox').waitAndClick();
         await transactionPage.transaction.DeleteTransactionBtn.waitAndClick();
@@ -41,6 +43,8 @@ describe("Basic tests for Expense Tracker Transaction page",()=> {
     })
 
     it("Verify that new transaction can be added with description ",async()=>{
+        transactionPage = pages('transactions');
+        await transactionPage.open();
         const count = await transactionPage.transaction.item('itemsCount').getText();    
         await transactionPage.transaction.AddNewTransactionBtn.waitAndClick();
         await transactionPage.addTransaction.input('amount').setValue('100');
