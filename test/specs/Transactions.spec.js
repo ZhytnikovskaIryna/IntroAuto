1b406ffa897073cc13afd390e2c828fca6b2771b
ZhytnkovskaIryna <iryna.zhytnikovska@gmail.com>
2023-10-18 14:50:57 +0300

    Test example that passes without await while it should FAIL

--- test/specs/Transactions.spec.js
+++ test/specs/Transactions.spec.js
@@ -5,37 +5,6 @@ describe("Basic tests for Expense Tracker Transaction page",()=> {
         transactionPage = pages('transactions');
         await transactionPage.open();
      });
-    it("Verify that modal window is opened when Add transaction is clicked on Transaction page",async()=>{
-        await transactionPage.transaction.AddNewTransactionBtn.waitAndClick();
-        await transactionPage.transaction.item('modal').waitForDisplayed();
-        await expect(await transactionPage.transaction.item('modal')).toBeDisplayed();
-        await transactionPage.addTransaction.cancelButton.waitAndClick();
-
-    })
-
-    it("Verify that new transaction can be added at Transaction page and +1 is added to the total count of transactions ",async()=>{
-        const count = await transactionPage.transaction.item('itemsCount').getText();    
-        await transactionPage.transaction.AddNewTransactionBtn.waitAndClick();
-        await transactionPage.addTransaction.input('amount').setValue('100');
-        await transactionPage.addTransaction.addButton.waitAndClick();
-        await expect(await transactionPage.transaction.item('itemsCount')).toHaveText("("+(parseInt(count.slice(1,-7))+1).toString()+" items)");
-    })
-
-    it("Verify that transaction can be deleted at Transaction page total count of transactions is decreased ",async()=>{
-        const count = await transactionPage.transaction.item('itemsCount').getText();    
-        await transactionPage.transaction.item('checkbox').waitAndClick();
-        await transactionPage.transaction.DeleteTransactionBtn.waitAndClick();
-        await transactionPage.deleteTransactionDialog.DeleteDialogOkButton.waitAndClick();
-        await expect(await transactionPage.transaction.item('itemsCount')).toHaveText("("+(parseInt(count.slice(1,-7))-1).toString()+" items)");
-    })
-
-    it("Verify that transaction is not deleted if Cancel button is clicked in Delete Transaction dialog",async()=>{
-        const count = await transactionPage.transaction.item('itemsCount').getText();    
-        await transactionPage.transaction.item('checkbox').waitAndClick();
-        await transactionPage.transaction.DeleteTransactionBtn.waitAndClick();
-        await transactionPage.deleteTransactionDialog.DeleteDialogCancelButton.waitAndClick();
-        await expect(await transactionPage.transaction.item('itemsCount')).toHaveText(count);
-    })
 
     it("Verify that new transaction can be added with description ",async()=>{
         const count = await transactionPage.transaction.item('itemsCount').getText();    
@@ -43,12 +12,8 @@ describe("Basic tests for Expense Tracker Transaction page",()=> {
         await transactionPage.addTransaction.input('amount').setValue('100');
         await transactionPage.addTransaction.input('description').setValue('description random');
         await transactionPage.addTransaction.addButton.waitAndClick();
-        await expect(await transactionPage.transaction.item('description')).toHaveText('description random');
+        // Here if we put await before expect test starts to fail as it should.Wothout await it passes though description I've put is wrong
+        expect(await transactionPage.transaction.item('description')).toHaveText('2description random');
     })
 
-    it("Verify that checkbox is clickable",async()=>{
-        const elem = await transactionPage.transaction.item('checkbox');   
-        await expect(elem).toBeClickable();
-        
-    })
 })
